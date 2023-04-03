1d5ec5c704737d03c3a8cdd3b5af33052b7a2f2e
Umair Shaikh <umairjr1265@gmail.com>
2023-04-04 03:57:21 +0530

    Adding heading in quote page

--- sections/Quotes.jsx
+++ sections/Quotes.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { TitleText, TypingText } from '../components';
 import SingleQuote from './SingleQuote';
 import styles from '../styles';
-import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
 
 const naruto_quote = "“If you don't like your destiny, don't accept it. Instead, have the courage to change it the way you want it to be.”";
 const fuegoleon_quote = "“Being weak is nothing to be ashamed of… Staying weak is !!“"
@@ -12,13 +12,15 @@ const kaori_quote = "“Maybe there's only a dark road up ahead. But you still h
 
 const Quotes = () => (
   <section className={`${styles.paddings}`}>
+    <TypingText title="| Anime Quotes" textStyles="text-center" />
+      <TitleText title={<>Quotes</>} textStyles="text-center" />
     <div className='gradient-04 z-0' />
     <SingleQuote title="Naruto Uzumaki" subtitle="(Naruto)" quote={naruto_quote} imgUrl="/naruto-quote.png" />
-    
+
     <SingleQuote title="Fuegoleon Vermillion" subtitle="(Black Clover)" quote={fuegoleon_quote} imgUrl="/fuegoleon-quote.png" />
-    
+
     <SingleQuote title="Mikasa Ackerman" subtitle="(Attack on Titan)" quote={mikasa_quote} imgUrl="/mikasa-quote.png" />
-    
+
     <SingleQuote title="Kaori Miyazono" subtitle="(Your lie in April)" quote={kaori_quote} imgUrl="/kaori-quote.png" />
   </section>
 );
